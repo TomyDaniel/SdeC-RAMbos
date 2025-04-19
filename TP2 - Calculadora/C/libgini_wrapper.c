d6beefb4884e94b9444755e6b648147e8cf43457
SofiViale <132108227+SofiViale@users.noreply.github.com>
2025-04-18 22:47:21 -0300

    Agregar función procesar_gini_final para procesar el valor de Gini utilizando ensamblador

--- TP2 - Calculadora/C/libgini_wrapper.c
+++ TP2 - Calculadora/C/libgini_wrapper.c
@@ -3,3 +3,8 @@ extern float sumar_uno(float valor);
 extern float promedio(float a, float b);
 extern float multiplicar(float a, float b);
 extern float dividir(float a, float b);
+extern int procesar_gini_asm(float valor_gini);
+
+int procesar_gini_final(float valor_gini) {
+    return procesar_gini_asm(valor_gini);
+}
